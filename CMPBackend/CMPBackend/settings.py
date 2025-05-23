da8ebc5f711d511152cd2ac9d588949a214d50a6
Adrian <bytrianes80@gmail.com>
2025-05-23 18:45:45 -0400

    Creando las carpetas de las apps

--- CMPBackend/CMPBackend/settings.py
+++ CMPBackend/CMPBackend/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'task_api',
+    'users',
+    'cryptotools',
+    'cryptolearn',
+    'blogmine',
 ]
 
 MIDDLEWARE = [
